3b126a7b0b3b975cf8e69160afe2b38118547056
Tate Keller Moctezuma <tatek1993@gmail.com>
2021-01-30 17:09:35 -0600

    test animation for checkmark in modal working

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import './Modal.scss'
+import Correct from '../Images/Correct.js';
+import Incorrect from '../Images/Incorrect.js';
 const Modal = props => {
 
     return (
                 <div className={`feedback ${props.data.correct ? 'correct hvr-pulse-grow' : 'incorrect hvr-icon-buzz-out'}`}>
                     <div className='answer'>
                         <div className='answerHeader'>
-                            <img></img>
+                            <Correct/>
                         </div>
                         <h1>
                             {props.data.header}
